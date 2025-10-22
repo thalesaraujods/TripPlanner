8fd2c9bcd2d5111f5db359d72b55efec6048a0b8
Thales Souza <t.araujods@gmail.com>
2025-10-22 14:37:47 -0400

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Thales Araújo on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
